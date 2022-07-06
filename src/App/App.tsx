d1cb63c6d0e9545af2451dcef634a519274f0ca1
mpvnlv <v.kruk@innopolis.university>
2022-07-07 01:44:05 +0300

    reglog page start

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -5,6 +5,9 @@ import React from 'react'
 import { Meme } from '../Index_page/Meme/Meme'
 import { MemeGenerated } from '../Index_page/MemeGenerated/MemeGenerated'
 import { Index } from '../Index_page/Index'
+import {Registration} from "../Index_page/Registration/reglog"
+import Login from "../Index_page/Registration/components/login.component"
+import SignUp from '../Index_page/Registration/components/signup.component'
 
 export function App() {
   return (
@@ -12,6 +15,10 @@ export function App() {
       <Route path='/' element={<Index />} />
       <Route path='/meme_page/generated' element={<MemeGenerated />} />
       <Route path='/meme_page' element={<Meme />} />
+      <Route path='/reglog/*' element={<Registration />}></Route>
+      <Route path="/reglog/sign-in" element={<Login />} />
+      <Route path="/reglog/sign-up" element={<SignUp />} />
+      
     </Routes>
   )
 }
