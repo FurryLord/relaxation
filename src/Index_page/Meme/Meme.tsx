e39efd31a545b487501207800edc30f9c9d28624
rusin <rusin.senya@mail.ru>
2022-07-07 12:56:19 +0300

    Add styles to meme_page

--- src/Index_page/Meme/Meme.tsx
+++ src/Index_page/Meme/Meme.tsx
@@ -1,20 +1,80 @@
 import React, { ChangeEvent, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-
 import CSS from 'csstype'
+import { Navibar } from '../Navbar/Navibar'
+import { NavibarBack } from '../Navbar/NaviBarBack'
 
 export function Meme() {
-  const container: CSS.Properties = {
-    marginTop: '50px',
+
+
+  const MemContainer: CSS.Properties = {
+    boxSizing: 'border-box',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+    width: '100%',
+    height: 'max-content',
+    background: '#FEEAC5',
+  }
+
+  const MemContent: CSS.Properties = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    marginTop: '2%',
+    marginBottom: '2%',
+    padding: '2% 5% 2% 5%',
+    border: '1px solid #000000',
+    width: 'max-content',
+    height: 'max-content',
+
+  }
+
+  const MemImage: CSS.Properties = {
+    height: 'max-content',
+    width: 'max-content',
+    marginTop: '2%',
+    marginBottom: '2%'
+  }
+
+  const EnterButton: CSS.Properties = {
+    background: '#FFFFFF',
+    border: '2px solid #000000',
+    borderRadius: '60px',
+
   }
 
   const skip: CSS.Properties = {
-    backgroundColor: '#f0ad4e',
+    borderStyle: 'none',
+    background: '#FEEAC5',
+    color: 'black',
+
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: '32px',
+    lineHeight: '38px',
+    marginBottom: '0px'
   }
 
   const generate: CSS.Properties = {
-    backgroundColor: '#0275d8',
+    background: '#A34AA7',
+    border: '1px solid #000000',
+    boxShadow: '-5px 5px 0px #000000',
+    borderRadius: '60px',
+
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: '32px',
+    lineHeight: '38px'
   }
+
+
 
   const [memes, setMemes] = useState<any[]>([])
   const [memeIndex, setMemIndex] = useState<any[number]>(0)
@@ -23,7 +83,7 @@ export function Meme() {
   const navigate = useNavigate()
 
   const updateCaption = (e: ChangeEvent<HTMLInputElement>, index: number) => {
-    const text = e.target.value || ''
+    const text = e.target.value
     setCaptions(
       captions.map((c, i) => {
         if (index === i) {
@@ -60,8 +120,9 @@ export function Meme() {
       body: formData,
     }).then((res) => {
       res.json().then((res) => {
-        navigate('/')
-        navigate(`meme_page/generated?url=${res.data.url}`)
+        const saveURL = window.location.pathname
+        console.log(saveURL)
+        navigate(`${saveURL}/generated?url=${res.data.url}`)
       })
     })
   }
@@ -87,18 +148,23 @@ export function Meme() {
   // }, [captions])
 
   return memes.length ? (
-    <div style={container}>
-      <button onClick={() => generateMeme()} style={generate}>
-        Generate
-      </button>
-      <button onClick={() => setMemIndex(memeIndex + 1)} style={skip}>
-        Skip
-      </button>
-      {captions.map((c, index) => (
-        <input onChange={(e) => updateCaption(e, index)} key={index} />
-      ))}
-      <img src={memes[memeIndex].url} alt='meme' />
-    </div>
+    <><NavibarBack />
+      <div style={MemContainer}>
+        <div style={MemContent}>
+
+          {captions.map((c, index) => (
+            <input onChange={(e) => updateCaption(e, index)} key={index} style={EnterButton} />
+          ))}
+          <button onClick={() => generateMeme()} style={generate}>
+            Generate
+          </button>
+          <img src={memes[memeIndex].url} alt='meme' style={MemImage} />
+          <button onClick={() => setMemIndex(memeIndex + 1)} style={skip}>
+            Skip
+          </button>
+        </div>
+      </div>
+    </>
   ) : (
     <></>
   )
---
+++ src/Index_page/Navbar/NaviBarBack.tsx
@@ -0,0 +1,86 @@
+import { useNavigate } from 'react-router-dom'
+import CSS from 'csstype'
+// eslint-disable-next-line import/prefer-default-export
+export function NavibarBack() {
+    const navigate = useNavigate()
+    const nav: CSS.Properties = {
+        backgroundColor: '#FEE9C6',
+        /* Size */
+        width: '100%',
+        height: 'fit-content',
+
+        /* Display */
+        display: 'flex',
+        flexDirection: 'row',
+        alignContent: 'center',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+
+        /* Others */
+        borderBottom: '2px solid #000000',
+    }
+    const logoBox: CSS.Properties = {
+        backgroundColor: '#FEE9C6',
+
+        height: 'fit-content',
+        width: 'fit-content',
+        /* Display */
+        display: 'flex',
+        alignContent: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        placeContent: 'center',
+        flexDirection: 'row',
+        flexWrap: 'nowrap',
+    }
+    const logoText: CSS.Properties = {
+        backgroundColor: '#FEE9C6',
+        color: '#000000',
+        fontFamily: 'Parisienne',
+        fontSize: '48px',
+
+        /* Size */
+        // height: 'fit-content',
+        // width: 'fit-content',
+
+        /* Display */
+        display: 'flex',
+        alignContent: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        placeContent: 'center',
+        flexDirection: 'row',
+        flexWrap: 'nowrap',
+
+        /* Others */
+        margin: '0',
+        marginLeft: '15px',
+        padding: '0',
+    }
+
+    const HomeButton: CSS.Properties = {
+        border: '1px solid #000000',
+        width: 'fit-content',
+        margin: '0% 0% 0% 5%',
+        padding: '0% 13% 0% 13%',
+        textAlign: 'center',
+
+
+        fontFamily: 'Roboto',
+        fontStyle: 'normal',
+        fontWeight: '400',
+        fontSize: '1.5em',
+        lineHeight: '2em',
+        backgroundColor: '#FEE9C6',
+        color: 'black'
+    }
+
+    return (
+        <header style={nav}>
+            <div style={logoBox}>
+                <button onClick={() => navigate('/')} style={logoText}>Relaxacion</button>
+                <button onClick={() => navigate('/reglog/*')} style={HomeButton} >Home</button>
+            </div>
+        </header>
+    )
+}
